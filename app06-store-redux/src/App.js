3164c9db9439ebb2b11c47c41bdb078998dd44da
GharrowBM <gharrow.games@gmail.com>
2021-10-28 14:20:26 +0200

    28-10-21

--- app06-store-redux/src/App.js
+++ app06-store-redux/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css';
-import {Provider} from 'react-redux'
 import Counter from './components/Counter';
 import TodoList from './components/TodoList';
 
@@ -9,11 +8,8 @@ function App() {
 
   return (
     <div className="App">
-      <Provider>
-                <Counter />        
+        <Counter />        
         {/* <TodoList /> */}
-      </Provider>
-
 
     </div>
   );
