da40cce95e2358e10c18effb78255d8aa3af8fe7
GharrowBM <gharrow.games@gmail.com>
2021-10-30 15:24:57 +0200

    Securizing keys

--- tp-weather/src/store/weatherSlices.js
+++ tp-weather/src/store/weatherSlices.js
@@ -1,13 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from 'axios'
 
-const appKey = '906c2c6bf0e311685adc38deee2e240e'
+const API_KEY = process.env.REACT_APP_API_KEY
 
 export const fetchWeatherAction = createAsyncThunk(
     'weather/fetch',
     async (payload, {rejectWithValue, getState,dispatch}) => {
         try {
-            const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${payload.city},${payload.countryCode}&appid=${appKey}&lang=fr`)
+            const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${payload.city},${payload.countryCode}&appid=${API_KEY}&lang=fr`)
             return data
         } catch (error) {
             if(!error?.response) {
